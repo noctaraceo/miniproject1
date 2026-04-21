7e2cad21de8534be6dc00caf113ee12f1928991e
noctaraceo <iliekgames10@gmail.com>
2026-04-21 17:44:24 +0600

    subtract added

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ def add(a,b):
     except Exception as e:
         print(f"error while adding: {e}")
 
+def subtract(a,b):
+    try:
+        return a - b
+    except Exception as e:
+        print(f"error while subtracting: {e}")
+    except TypeError as e:
+        print(f"error while subtracting: {e}")
+
+if __name__ == '__main__':
+    print(add(2,3))
+    print(subtract(8,67))
+    print(subtract("hello", 5))
--- testing.py
+++ testing.py
@@ -4,6 +4,9 @@ import unittest
 class TestMain(unittest.TestCase):
     def test_add(self):
         self.assertEqual(main.add(2, 3), 5)
-
+    def test_sub(self):
+        self.assertEqual(main.subtract(8,67), -59)
+        
+    
 if __name__ == '__main__': 
     unittest.main()
